270932110176a6caf89a981953dacf6d324fea4f
kgoins <kgoins@github.com>
2015-10-28 03:57:49 -0400

    added vim config script

---
+++ vim_config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "installing pathogen"
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+echo "installing delimitmate"
+cd ~/.vim/bundle
+git clone https://github.com/Raimondi/delimitMate.git
+
+echo "installing syntastic"
+cd ~/.vim/bundle && \
+git clone https://github.com/scrooloose/syntastic.git
