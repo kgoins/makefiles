36cd969b799169cdc4b7fd46c9d89f42fa8818f0
kgoins <goins.kyle@ufl.edu>
2015-11-02 19:02:18 -0500

    added Vim-Commentary to vim setup

--- vim_config.sh
+++ vim_config.sh
@@ -15,3 +15,7 @@ git clone https://github.com/scrooloose/syntastic.git
 echo "installing easyMotion"
 cd ~/.vim/bundle && \
 git clone https://github.com/easymotion/vim-easymotion.git
+
+echo "installing Commentary"
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-commentary.git
