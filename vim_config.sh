e39ec24571de93595a40801b83de9d4a8c79e3f4
kgoins <kgoins@github.com>
2015-11-29 00:41:38 -0500

    freebsd comit

--- vim_config.sh
+++ vim_config.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# todo: 
+# - check to see if bundle dir exists
+# - check to see if each plugin is already loaded by looking at the string
+#     directly preceeding the .git but before the / via regex etc.
+# - add ability to select which plugins to install
+
 echo "installing pathogen"
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
@@ -19,3 +25,9 @@ git clone https://github.com/easymotion/vim-easymotion.git
 echo "installing Commentary"
 cd ~/.vim/bundle
 git clone git://github.com/tpope/vim-commentary.git
+
+echo "installing Snipmate"
+cd ~/.vim/bundle
+git clone https://github.com/tomtom/tlib_vim.git
+git clone https://github.com/MarcWeber/vim-addon-mw-utils.git
+git clone https://github.com/garbas/vim-snipmate.git
